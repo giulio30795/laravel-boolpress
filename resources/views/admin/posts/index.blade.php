06248167757a02644d90e241ca08887700858d4e
giulio30795 <giulioincarbone95@gmail.com>
2022-01-31 17:06:44 +0100

    done

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,11 +12,18 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($posts as $post){
+            @foreach($posts as $post)
                 <tr>
                     <td>{{$post->id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>
+                        <button class="btn btn-primary">View Details</button>
+                        <button class="btn btn-warning">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
                 </tr>
-            }
+            @endforeach
+            
         </tbody>
     </table>
 </div>
