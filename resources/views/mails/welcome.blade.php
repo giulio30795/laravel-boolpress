8bdaa7ab983f2a7c0d56c46724842b115ff0134c
giulio30795 <giulioincarbone95@gmail.com>
2022-02-14 17:40:26 +0100

    test email

---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,5 @@
+<h1>Welcome {{$name}}</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio harum repellendus obcaecati nisi! Nobis facilis repellendus eius. Id aperiam eaque harum blanditiis itaque rerum cumque, dolores ipsa dolorem nobis.
+</p>
