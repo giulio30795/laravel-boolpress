28c9bb9c27e9abc0cc0d54ff0c59117d72e695c0
giulio30795 <giulioincarbone95@gmail.com>
2022-02-14 17:53:11 +0100

    email done

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index () {
+
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail(Auth::user()->name));
+
+
         return view('admin.home');
     }
 }
